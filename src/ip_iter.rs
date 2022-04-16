b5c77979d8bfe5b77e8bd23344e3365ab924b722
Connor Slade <connor@connorcode.com>
2022-04-16 11:35:54 -0400

    🥞 Ststus Printing

--- src/ip_iter.rs
+++ src/ip_iter.rs
@@ -1,5 +1,8 @@
 use std::fmt::{self, Display};
+use std::io;
 use std::iter::Iterator;
+use std::net::{Ipv4Addr, SocketAddr, SocketAddrV4, ToSocketAddrs};
+use std::vec;
 
 // a.b.c.d
 #[derive(Debug, Clone, Copy)]
@@ -25,12 +28,27 @@ impl Iterator for IpIter {
     type Item = Self;
 
     fn next(&mut self) -> Option<Self> {
+        if self.a == 255 && self.b == 255 && self.c == 255 && self.d == 255 {
+            return None;
+        }
+
         self.d += 1;
         max_reset(&mut self.c, &mut self.d);
         max_reset(&mut self.b, &mut self.c);
         max_reset(&mut self.a, &mut self.b);
 
         Some(self.clone())
+    }
+}
+
+impl ToSocketAddrs for IpIter {
+    type Iter = vec::IntoIter<SocketAddr>;
+    fn to_socket_addrs(&self) -> io::Result<Self::Iter> {
+        Ok(vec![SocketAddr::V4(SocketAddrV4::new(
+            Ipv4Addr::new(self.a, self.b, self.c, self.d),
+            80,
+        ))]
+        .into_iter())
     }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,63 @@
+use std::io::{stdout, Write};
+use std::sync::{
+    atomic::{AtomicUsize, Ordering},
+    mpsc,
+};
+use std::thread;
+
+use ureq;
+
 mod ip_iter;
+
+const IP_COUNT: usize = 4294967296;
+const DISPLAY_PER: usize = 100;
 
 fn main() {
     let ips = ip_iter::IpIter::new();
-    for i in ips.into_iter() {}
+    let ip_count = AtomicUsize::new(0);
+    let (rx, tx) = mpsc::channel();
+
+    thread::spawn(move || {
+        for i in tx {
+            ip_count.fetch_add(1, Ordering::Relaxed);
+            let current = ip_count.load(Ordering::Relaxed);
+            let clean_current = current / IP_COUNT * DISPLAY_PER;
+
+            print!(
+                "\x1B[2K\r[{}{}] {}",
+                "*".repeat(clean_current),
+                " ".repeat(DISPLAY_PER - clean_current),
+                i
+            );
+            stdout().flush().unwrap();
+        }
+    });
+
+    for i in ips.into_iter() {
+        rx.send(i).unwrap();
+        let res = match ureq::get(&format!("http://{}/", i)).call() {
+            Ok(i) => i,
+            Err(_) => continue,
+        };
+
+        dbg!(res);
+    }
 }
+
+// fn main() {
+//     let timeout = Duration::from_secs(5);
+//     let ips = ip_iter::IpIter::new();
+//
+//     let req = Request::builder()
+//         .version(Version::HTTP_11)
+//         .method("GET")
+//         .header("User-Agent", "Internet-Info")
+//         .body(())
+//         .unwrap();
+//
+//     for i in ips.into_iter() {
+//         let addr = i.to_socket_addrs().unwrap().next().unwrap();
+//         let stream = TcpStream::connect_timeout(&addr, timeout).unwrap();
+//         stream.write_all(req).unwrap();
+//     }
+// }
